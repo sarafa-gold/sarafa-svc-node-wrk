44e74a4345ba1e60679f02432184b9ace8b7a50c
paragmore <paragmore306@gmail.com>
2024-07-05 14:29:37 +0530

    types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,21 @@
-interface CmdOptions {
-    env: 'production' | 'development';
-    debug: boolean;
-}
+// index.d.ts
 
-interface WorkerHandler {
+import * as path from "path";
+
+// Define the WorkerHandler interface
+export interface WorkerHandler {
     active: boolean;
     stop(callback: () => void): void;
 }
 
+// Define the CmdOptions interface
+export interface CmdOptions {
+    env: 'production' | 'development';
+    debug: boolean;
+    serviceRoot?: string;
+}
+
+// Declare the worker function
 declare function worker(cmd: CmdOptions): WorkerHandler;
 
-declare module 'sarafa-svc-node-wrk' {
-    const cmd: CmdOptions;
-    const hnd: WorkerHandler;
-    export = hnd;
-}
+export default worker;
